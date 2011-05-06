c2e8584247e333edf70f1d0837f821145ab40793
Unkid4n <Unkid4n@gmail.com>
2011-05-06 15:36:59 -0700

    Auction Behavior updated. Forced Dice roll of 12 in Game Master to test auction square. REMOVE if necessary

--- mc/AuctionBehavior.java
+++ mc/AuctionBehavior.java
@@ -1,15 +1,62 @@
 package mc;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+
 public class AuctionBehavior implements SquareBehavior {
 
+	GameMaster gameMaster;
+	GamePane gamePane;
+	ArrayList<District> districts;
+	ArrayList<District> unownedDistricts;
+	boolean failed;
+	
 	public AuctionBehavior() {
-		// TODO Auto-generated constructor stub
+	gamePane = GamePane.getInstance();
+	failed = false;
 	}
 
 	@Override
 	public void execute() {
-		GamePane.getInstance().setMessagePanelText("You landed on Auction");
+		gameMaster = GameMaster.getInstance();
+		gamePane.clearMessageLayer();
+//		districts = gameMaster.getBoard().getDistricts();
+		if(!failed)	gamePane.setMessagePanelText("You landed on Auction");
+		else gamePane.setMessagePanelText("District Already Owned.");
+		gamePane.addMessagePanelText("Select an Unowned District to Auction.");
+		
+//		for(District district: districts){
+//			if(district.isOwned() == false)
+//				unownedDistricts.add(district);
+//		}
 
+		gamePane.addSelectionLayer();
+		gamePane.setSelectedDistrict(0);
+		JButton startAuctionButton = new JButton("Start Auction");
+		startAuctionButton.addActionListener(
+				new ActionListener(){
+					public void actionPerformed(ActionEvent evt){
+						startAuctionButtonPerformed();
+					}
+				});
+		gamePane.addMessagePanelButton(startAuctionButton);
 	}
 
-}
+	protected void startAuctionButtonPerformed() {
+		int selectedDistrictIndex = gamePane.getSelectedDistrict();
+		Board board = gameMaster.getBoard();
+			Square selectedSquare = board.getSquare(selectedDistrictIndex);
+			District district;
+			if(selectedSquare.getType() == SQUARETYPE.DISTRICT){
+				district = (District)selectedSquare;
+				if(!district.isOwned())
+					gameMaster.startAuction();
+				else{
+					failed = true;
+					execute();
+			}}
+		}	
+	}
